27c4e26cfd8f4d95b74766ce5e6f11e23794787f
sorokinaolga <sorokinaop@list.ru>
2026-07-11 22:18:52 +0300

    init

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <header>Заголовок</header>
+      <section>Таблица</section>
+      <footer>Подвал</footer>
+    </>
+  )
+}
+
+export default App
