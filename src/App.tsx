7db2ef0aaa7cb24ca9cfcf6ce9e10f94053a22d8
sorokinaolga <sorokinaop@list.ru>
2026-07-12 22:47:54 +0300

    add eslint + prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import './App.css'
+import './App.css';
 
 function App() {
   return (
@@ -7,7 +7,7 @@ function App() {
       <section>Таблица</section>
       <footer>Подвал</footer>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
